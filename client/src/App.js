cf797aaee3835cdc0960429be223bc422c3455bb
DESKTOP-JIU76K2\chj <guswo5580@gmail.com>
2019-04-24 01:28:34 +0900

    Apply progressbar

--- client/src/App.js
+++ client/src/App.js
@@ -7,14 +7,17 @@ import TableHead from '@material-ui/core/TableHead'
 import TableBody from '@material-ui/core/TableBody'
 import TableRow from '@material-ui/core/TableRow'
 import TableCell from '@material-ui/core/TableCell'
+import CircularProgress from '@material-ui/core/CircularProgress'
 import {withStyles} from '@material-ui/core/styles'
 
 class App extends Component {
   state = {
-    customers : ""
+    customers : "",
+    completed : 0
   }
 
   componentDidMount(){
+    this.timer = setInterval(this.progress, 20);
     this.callApi()
       .then( res => this.setState({customers : res }))
       .catch( err => console.log(err))
@@ -23,6 +26,11 @@ class App extends Component {
     const response = await fetch('/api/customers');
     const body = await response.json();
     return body;
+  }
+
+  progress = () => {
+    const {completed} = this.state;
+    this.setState({completed : completed >=100 ? 0 : completed+1});
   }
 
   render(){
@@ -55,7 +63,11 @@ class App extends Component {
                   job={c.job}
                 />
               }) : 
-              "Loading... "
+              <TableRow>
+                <TableCell colSpan="6" align="center">
+                  <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+                </TableCell>
+              </TableRow>
             }
           </TableBody>
         </Table>
@@ -72,6 +84,9 @@ const styles = thema => ({
   },
   table: {
     minWidth : 1000
+  },
+  progress : {
+    margin : thema.spacing.unit*2
   }
 })
 
