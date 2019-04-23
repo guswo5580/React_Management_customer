8529f23574fc480eaac04a9b0239bdd15c0cfd33
DESKTOP-JIU76K2\chj <guswo5580@gmail.com>
2019-04-24 02:41:26 +0900

    setting aws RDS and using mysql query to connect to db

--- server.js
+++ server.js
@@ -1,36 +1,32 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended : true }))
+app.use(bodyParser.urlencoded({extended : true }));
+
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host : conf.host,
+    user : conf.user,
+    password : conf.password,
+    port : conf.port,
+    database : conf.database
+});
+connection.connect();
 
 app.get('/api/customers' , (req, res) => {
-    res.send([
-        {
-            id : 1,
-            img : 'https://placeimg.com/64/64/1',
-            name : '조현재',
-            birthday : '940308',
-            gender : '남',
-            job : '학생'
-        },{
-            id : 2,
-            img : 'https://placeimg.com/64/64/2',
-            name : '조현재',
-            birthday : '940308',
-            gender : '남',
-            job : '학생'
-        },{
-            id : 3,
-            img : 'https://placeimg.com/64/64/3',
-            name : '조현재',
-            birthday : '940308',
-            gender : '남',
-            job : '학생'
-      }
-    ])
+    connection.query(
+        "SELECT * FROM CUSTOMER", 
+        (err, rows, fields) => {
+            res.send(rows);
+        }
+    );
 });
 
 app.listen(port, () => {
