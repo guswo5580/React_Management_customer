2f96e502f776de97d5a82e61e96883ca75a2c959
DESKTOP-JIU76K2\chj <guswo5580@gmail.com>
2019-04-25 01:59:06 +0900

    send data from server to dbs, install multer for imageFile

--- server.js
+++ server.js
@@ -20,6 +20,9 @@ const connection = mysql.createConnection({
 });
 connection.connect();
 
+const multer = require('multer');
+const upload = multer({dest : './uploads'});
+
 app.get('/api/customers' , (req, res) => {
     connection.query(
         "SELECT * FROM CUSTOMER", 
@@ -27,6 +30,25 @@ app.get('/api/customers' , (req, res) => {
             res.send(rows);
         }
     );
+});
+
+app.use('/image', express.static('./uploads'));
+//사용자는 image 폴더를 이용하지만, 실제로는 express 의 upload 폴더에 매핑이 됨
+app.post('/api/customers', upload.single('image'), (req, res) => {
+    let sql = 'INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?)';
+    let image = '/image/' + req.file.filename;
+    let name = req.body.name;
+    let birthday = req.body.birthday;
+    let gender = req.body.gender;
+    let job = req.body.job;
+    
+    let params = [image, name, birthday, gender, job];
+    //sql문의 빈칸을 params으로 담아 보냄
+
+    connection.query(sql, params, (err, rows, fields) => {
+        res.send(rows);
+        console.log(err)
+    });
 });
 
 app.listen(port, () => {
